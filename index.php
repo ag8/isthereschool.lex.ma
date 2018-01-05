948f7be91674309ed041578ed9f4152c6ac1884c
Derik K <derikk@users.noreply.github.com>
2018-01-04 23:59:10 -0500

    Version 0.1.1
    Switch to WCVB for closings data (thanks Louie!), add Concord-Carlisle high school, arrange towns geographically, add easter egg

--- index.php
+++ index.php
@@ -1,19 +1,10 @@
 <?php
-	$cbspage = file_get_contents('http://boston.cbslocal.com/closings/');
-	$lines = explode("\n", $cbspage);
-	
-	$good_lines = preg_grep ( "<div class=\"search-wrapper\">", $lines );
-	
-	$good_line = array_pop(array_reverse($good_lines));
-	
-	
-	
-	preg_match_all("/<div class=\"business\"><p class=\"name\">(.*?)<\/p><p class=\"county\">(.*?)<\/p><p class=\"category\">(.*?)<\/p><\/div><p class=\"status red\">(.*?)<\/p><\/div>/", $good_line, $regex_trash);
-	
-	$actual_array = array_pop(array_reverse($regex_trash));
-	
-	//var_dump( $actual_array );
-	
+	$wcvbpage = file_get_contents('http://www.wcvb.com/weather/closings');
+
+	preg_match_all("/<div class=\"weather-closings-data-item\" data-name=\"(.*?)\" data-count=\"(.*?)\">\s*<div class=\"weather-closings-data-name\">(.*?)<\/div>\s*<div class=\"weather-closings-data-location\">\s*(.*?)<br>\s*<\/div>\s*<div class=\"weather-closings-data-status\">\s*(<ul class=\"weather-closings-data-status-list\">\s*(<li class=\"weather-closings-data-status-list-item\">(.*?)<\/li>\s*)*<\/ul>|(.*?))\s*<\/div>\s*<\/div>/", $wcvbpage, $wcvb_trash);
+
+	$wcvb_array = array_pop(array_reverse($wcvb_trash));
+
 	$statuses = array();
 	$statuses[0] = "Open";
 	$statuses[1] = "Open";
@@ -23,45 +14,50 @@
 	$statuses[5] = "Open";
 	$statuses[6] = "Open";
 	$statuses[7] = "Open";
+	$statuses[8] = "Open";
 	
 	
 	$lexington = 'green';
+	$closed = false;
 	
-	foreach ($actual_array as $value) {
-		// echo $value;
-		
-		$name = get_string_between( $value, "<p class=\"name\">", "</p><p class=\"county\">");
-		$what = get_string_between( $value, "<p class=\"status red\">", "</p></div>");
-		
-		if ($name == 'Arlington') {
+	foreach ($wcvb_array as $value) {
+
+		$name = get_string_between( $value, "<div class=\"weather-closings-data-name\">", "</div>");
+			$what = get_string_between( $value, "<div class=\"weather-closings-data-status\">", "</div>");
+
+		if ($name == 'Burlington Public') {
 			$statuses[0] = $what;
 		}
-		if ($name == 'Bedford') {
+		if ($name == 'Woburn Public') {
 			$statuses[1] = $what;
 		}
-		if ($name == 'Belmont') {
+		if ($name == 'Winchester Public') {
 			$statuses[2] = $what;
 		}
-		if ($name == 'Burlington') {
-			$statuses[3] = $what;
-		}
-		if ($name == 'Lincoln') {
+				if ($name == 'Arlington Public') {
+					$statuses[3] = $what;
+				}
+		if ($name == 'Belmont Public') {
 			$statuses[4] = $what;
 		}
-		if ($name == 'Waltham') {
+		if ($name == 'Waltham Public') {
 			$statuses[5] = $what;
 		}
-		if ($name == 'Winchester') {
+		if ($name == 'Lincoln Public') {
 			$statuses[6] = $what;
 		}
-		if ($name == 'Woburn') {
+		if ($name == 'Concord - Carlisle Regional') {
 			$statuses[7] = $what;
 		}
-		if ($name == 'Lexington') {
+		if ($name == 'Bedford Public') {
+			$statuses[8] = $what;
+		}
+		if ($name == 'Lexington Public') {
 			$lexington = 'red';
+						$closed = true;
 		}
 	}
-	
+
 	//var_dump( $statuses );
 	
 	//echo (strpos(strtolower($statuses[0]), 'closed') !== false ? 'green' : 'red');
@@ -70,81 +66,85 @@
 	
 	// Based on https://stackoverflow.com/a/9826656
 	function get_string_between($string, $start, $end){
-	    $string = ' ' . $string;
-	    $ini = strpos($string, $start);
-	    if ($ini == 0) return '';
-	    $ini += strlen($start);
-	    $len = strpos($string, $end, $ini) - $ini;
-	    return substr($string, $ini, $len);
+		$string = ' ' . $string;
+		$ini = strpos($string, $start);
+		if ($ini == 0) return '';
+		$ini += strlen($start);
+		$len = strpos($string, $end, $ini) - $ini;
+		return substr($string, $ini, $len);
 	}
 ?>
 
 
 <html>
-  <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
-      google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Arlington',     1],
-          ['Bedford',      1],
-          ['Belmont',  1],
-          ['Burlington', 1],
-          ['Lincoln', 1],
-          ['Waltham', 1],
-          ['Winchester', 1],
-          ['Woburn',    1]
-        ]);
+ <head>
+	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+	<script type="text/javascript">
+		google.charts.load("current", {packages:["corechart"]});
+		google.charts.setOnLoadCallback(drawChart);
+		function drawChart() {
+		var data = google.visualization.arrayToDataTable([
+		  ['Town', 'Angle'],
+		  ['Burlington', 3],
+		  ['Woburn', 2],
+		  ['Winchester', 2],
+		  ['Arlington', 2],
+		  ['Belmont', 2],
+		  ['Waltham', 4],
+		  ['Lincoln', 4],
+		  ['Concord', 2],
+		  ['Bedford', 3]
+		]);
 
-        var options = {
-          backgroundColor: '<?php echo $lexington; ?>',
-          pieHole: 0.4,
-          legend: 'none',
-          pieSliceText: 'label',
-          tooltip: {textStyle: {color: '#FF0000'}, showColorCode: false, isHTML: true, text: 'value', trigger: 'none'},
-          slices: {
-            0: { color: "<?php echo (strpos(strtolower($statuses[0]), 'closed') !== false ? 'red': 'green'); ?>" },
-            1: { color: "<?php echo (strpos(strtolower($statuses[1]), 'closed') !== false ? 'red': 'green'); ?>" },
-            2: { color: "<?php echo (strpos(strtolower($statuses[2]), 'closed') !== false ? 'red': 'green'); ?>" },
-            3: { color: "<?php echo (strpos(strtolower($statuses[3]), 'closed') !== false ? 'red': 'green'); ?>" },
-            4: { color: "<?php echo (strpos(strtolower($statuses[4]), 'closed') !== false ? 'red': 'green'); ?>" },
-            5: { color: "<?php echo (strpos(strtolower($statuses[5]), 'closed') !== false ? 'red': 'green'); ?>" },
-            6: { color: "<?php echo (strpos(strtolower($statuses[6]), 'closed') !== false ? 'red': 'green'); ?>" },
-            7: { color: "<?php echo (strpos(strtolower($statuses[7]), 'closed') !== false ? 'red': 'green'); ?>" }
-          },
-          sliceVisibilityThreshold: 0.125,
-          chartArea:{width:'90%',height:'90%'}
-        };
+		var options = {
+		  backgroundColor: '<?php echo $lexington; ?>',
+		  pieHole: 0.4,
+		  legend: 'none',
+		  pieSliceText: 'label',
+		  tooltip: {textStyle: {color: '#FF0000'}, showColorCode: false, isHTML: true, text: 'value', trigger: 'none'},
+		  slices: {
+			0: { color: "<?php echo (strpos(strtolower($statuses[0]), 'closed') !== false ? 'red': 'green'); ?>" },
+			1: { color: "<?php echo (strpos(strtolower($statuses[1]), 'closed') !== false ? 'red': 'green'); ?>" },
+			2: { color: "<?php echo (strpos(strtolower($statuses[2]), 'closed') !== false ? 'red': 'green'); ?>" },
+			3: { color: "<?php echo (strpos(strtolower($statuses[3]), 'closed') !== false ? 'red': 'green'); ?>" },
+			4: { color: "<?php echo (strpos(strtolower($statuses[4]), 'closed') !== false ? 'red': 'green'); ?>" },
+			5: { color: "<?php echo (strpos(strtolower($statuses[5]), 'closed') !== false ? 'red': 'green'); ?>" },
+			6: { color: "<?php echo (strpos(strtolower($statuses[6]), 'closed') !== false ? 'red': 'green'); ?>" },
+			7: { color: "<?php echo (strpos(strtolower($statuses[7]), 'closed') !== false ? 'red': 'green'); ?>" },
+			8: { color: "<?php echo (strpos(strtolower($statuses[8]), 'closed') !== false ? 'red': 'green'); ?>" }
+		  },
+		  chartArea:{width:'90%',height:'90%'}
+		};
 
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        chart.draw(data, options);
-      }
-    </script>
-    <style>
-    	html, body {
-    		margin: 0;
-    		padding: 0;
-    	}
-    	.container {
-   	    position: absolute;
-	    top: 50%;
-	    left: 50%;
-	    -moz-transform: translateX(-50%) translateY(-50%);
-	    -webkit-transform: translateX(-50%) translateY(-50%);
-	    transform: translateX(-50%) translateY(-50%);
-	    z-index: 10000;
-	    font-size: 36px;
-	    font-family: Georgia, serif;
+		var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+		chart.draw(data, options);
+  		}
+	</script>
+	<?php if($closed){echo('<script type="text/javascript" src="snowstorm.js"></script>');} ?>
+	<style>
+		html, body {
+			margin: 0;
+			padding: 0;
+		}
+		.container {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		-moz-transform: translateX(-50%) translateY(-50%);
+		-webkit-transform: translateX(-50%) translateY(-50%);
+		transform: translateX(-50%) translateY(-50%);
+		z-index: 10000;
+		font-size: 36px;
+		font-family: Georgia, serif;
 	}
-    </style>
-  </head>
-  <body>
-  <div class="container">
-	    <span>Lexington!</span>
+	</style>
+	<meta http-equiv="refresh" content="300">
+	<title>Is there school?</title>
+</head>
+<body>
+	<div class="container">
+		<span>Lexington<?php if($closed) { echo('!');} ?></span>
 	</div>
-    <div id="donutchart" style="width: 100%; height: 100%;"></div>
-  </body>
+	<div id="donutchart" style="width: 100%; height: 100%;"></div>
+</body>
 </html>
